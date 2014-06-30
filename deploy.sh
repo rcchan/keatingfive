2e6a022711aac46da756b1d8edc1ba47e7d5491d
Ryan Chan <ryan@rcchan.com>
2014-06-29 23:48:36 -0400

    Change dir to keatingfive

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ shopt -s dotglob
 rm -r build/*
 shopt -u dotglob
 grunt
-rsync -rLkpEtzPc --delete-delay /web/keating5/build/ rchan@keating:~/web
+rsync -rLkpEtzPc --delete-delay /web/keatingfive/build/ rchan@keating:~/web
