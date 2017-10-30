c6f6cb7e629590ff2ebd92fb075de56e72664f37
Ryan Chan <ryan@rcchan.com>
2017-10-30 00:11:43 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
+set -eo pipefail
 shopt -s dotglob
-rm -r build/*
+rm -r build/* || true
 shopt -u dotglob
 grunt
-rsync -rLkpEtzPc --delete-delay /web/keatingfive/build/ rchan@keating:~/web
+rsync -rLkpEtzPc --delete-delay build/ rcchan@keating:~/web/
