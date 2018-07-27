108c3bbca024587451048266ed95fcc0e36b4b51
Ryan Chan <ryan@rcchan.com>
2018-07-26 21:33:46 -0400

    Add build-only option to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 set -eo pipefail
 shopt -s dotglob
+
 rm -r build/* || true
 shopt -u dotglob
 grunt
-rsync -rLkpEtzPc --delete-delay build/ rcchan@keating:~/web/
+if [[ "$1" != "--build" ]]; then
+  rsync -rLkpEtzPc --delete-delay build/ rcchan@keating:~/web/
+fi
